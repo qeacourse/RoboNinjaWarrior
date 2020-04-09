f179c37687478c6d77464d3fdd4f029092454a84
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-09 09:10:31 -0400

    added bod starter code

---
+++ Sample_code/starterCodeForBridgeOfDoomQEA2020.m
@@ -0,0 +1,58 @@
+function starterCodeForBridgeOfDoomQEA2020()
+% Insert any setup code you want to run here
+
+% define u explicitly to avoid error when using sub functions
+% see: https://www.mathworks.com/matlabcentral/answers/268580-error-attempt-to-add-variable-to-a-static-workspace-when-it-is-not-in-workspace
+u = [];
+% u will be our parameter
+syms u;
+
+% this is the equation of the bridge
+R = 4*[0.396*cos(2.65*(u+1.4));...
+       -0.99*sin(u+1.4);...
+       0];
+
+% tangent vector
+T = diff(R);
+
+% normalized tangent vector
+That = T/norm(T);
+
+pub = rospublisher('raw_vel');
+
+% stop the robot if it's going right now
+stopMsg = rosmessage(pub);
+stopMsg.Data = [0 0];
+send(pub, stopMsg);
+
+bridgeStart = double(subs(R,u,0));
+startingThat = double(subs(That,u,0));
+placeNeato(bridgeStart(1),  bridgeStart(2), startingThat(1), startingThat(2));
+
+% wait a bit for robot to fall onto the bridge
+pause(2);
+
+% time to drive!!
+
+% For simulated Neatos only:
+% Place the Neato in the specified x, y position and specified heading vector.
+function placeNeato(posX, posY, headingX, headingY)
+    svc = rossvcclient('gazebo/set_model_state');
+    msg = rosmessage(svc);
+
+    msg.ModelState.ModelName = 'neato_standalone';
+    startYaw = atan2(headingY, headingX);
+    quat = eul2quat([startYaw 0 0]);
+
+    msg.ModelState.Pose.Position.X = posX;
+    msg.ModelState.Pose.Position.Y = posY;
+    msg.ModelState.Pose.Position.Z = 1.0;
+    msg.ModelState.Pose.Orientation.W = quat(1);
+    msg.ModelState.Pose.Orientation.X = quat(2);
+    msg.ModelState.Pose.Orientation.Y = quat(3);
+    msg.ModelState.Pose.Orientation.Z = quat(4);
+
+    % put the robot in the appropriate place
+    ret = call(svc, msg);
+end
+end
