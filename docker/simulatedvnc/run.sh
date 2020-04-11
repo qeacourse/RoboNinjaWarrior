27c36b854fca550d98882b8b605e3fb9613dadd6
paulruvolo <paullundyruvolo@gmail.com>
2020-04-11 08:17:10 -0400

    Update run.sh

--- docker/simulatedvnc/run.sh
+++ docker/simulatedvnc/run.sh
@@ -3,5 +3,5 @@
 docker stop neato;
 docker rm --force neato;
 # this is an example of running with specific port mappings for VNC and gzweb
-docker run --rm --name=neato --mac-address 00:25:90:8c:2e:38 --mount type=bind,source=/usr/local/MATLAB/,target=/usr/local/MATLAB,readonly -p 9000:8080 -p 5900:5900 -e NEATO_WORLD=bod_volcano -it test2
+docker run --rm --name=neato --mac-address 00:25:90:8c:2e:38 --mount type=bind,source=/usr/local/MATLAB/,target=/usr/local/MATLAB,readonly -p 9000:8080 -p 5900:5900 -e NEATO_WORLD=bod_volcano -it qeacourse/robodocker:simulatedvnc
 
