f8532efd0048d1d0a6ed528962ad3ba874b1fbfe
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-13 12:21:49 -0400

    added code optimized for local network

---
+++ docker/simulatedflexible/run_host.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this is an example of running with specific port mappings for VNC and gzweb using host networking mode
+docker run --rm --net=host -e GZWEB_PORT=8092 -e ROS_HOSTNAME=deepthought.olin.edu -e ROS_PORT=11312 -e GAZEBO_PORT=11346 -e NEATO_WORLD=empty_no_spawn -it test
+
