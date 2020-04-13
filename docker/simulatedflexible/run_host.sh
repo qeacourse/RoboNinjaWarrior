1f994ef49d2d04235490a7294792b76a013f8608
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-13 15:36:26 -0400

    made more flexible

--- docker/simulatedflexible/run_host.sh
+++ docker/simulatedflexible/run_host.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# this is an example of running with specific port mappings for VNC and gzweb using host networking mode
-docker run --rm --net=host -e GZWEB_PORT=8092 -e ROS_HOSTNAME=deepthought.olin.edu -e ROS_PORT=11312 -e GAZEBO_PORT=11346 -e NEATO_WORLD=empty_no_spawn -it qeacourse/robodocker:simulatedflexible
 
+if [ "$#" -ne 1 ]; then
+    echo "./run_host.sh instance-id"
+    exit 1
+fi
+let "ROS_PORT=11311 + $1"
+let "GAZEBO_PORT=11345 + $1"
+let "GZWEB_PORT=8900 + $1"
+
+# this is an example of running with specific port mappings for VNC and gzweb using host networking mode
+docker run --rm --net=host -e GZWEB_PORT=$GZWEB_PORT -e ROS_HOSTNAME=deepthought.olin.edu -e ROS_PORT=$ROS_PORT -e GAZEBO_PORT=$GAZEBO_PORT -e NEATO_WORLD=empty_no_spawn -it qeacourse/robodocker:simulatedflexible
