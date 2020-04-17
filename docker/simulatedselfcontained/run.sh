042304b856fa16f993df47eebdf74dca89b9b2a0
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-17 16:25:57 -0400

    fully self-contained environment

--- docker/simulatedselfcontained/run.sh
+++ docker/simulatedselfcontained/run.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 
-docker run --init --gpus=all  --rm -it  --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly --mac-address 00:25:90:8c:2e:38 -e DISPLAY=:1 -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -p 40001:40001 testpruvolo
+if [ "$#" -ne 1 ]; then
+    echo "./run.sh instance-id"
+    exit 1
+fi
+let "NO_VNC_PORT=40001 + $1"
+
+docker run --init --gpus=all  --rm -it --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -p $NO_VNC_PORT:40001 testpruvolo
