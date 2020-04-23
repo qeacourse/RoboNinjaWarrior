f11f101afd2ab4502999031d545a7be6cfd6499a
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-04-23 15:17:00 -0400

    startx if not running

--- docker/simulatedselfcontained/run.sh
+++ docker/simulatedselfcontained/run.sh
@@ -6,4 +6,10 @@ if [ "$#" -ne 1 ]; then
 fi
 let "NO_VNC_PORT=40001 + $1"
 
+if [[ ! -f /tmp/.X11-unix/X0 ]]; then
+    startx &
+    sleep 5
+fi
+
+
 docker run --init --gpus=all  --rm -it --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -p $NO_VNC_PORT:40001 qeacourse/robodocker:selfcontained
