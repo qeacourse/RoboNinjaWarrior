f6d87cc4d66cec97a7beaac6a1b6c263c555a88e
paulruvolo <paullundyruvolo@gmail.com>
2020-04-24 16:47:18 -0400

    don't do interactive

--- docker/simulatedselfcontained/run.sh
+++ docker/simulatedselfcontained/run.sh
@@ -12,4 +12,4 @@ if [[ ! -S /tmp/.X11-unix/X0 ]]; then
 fi
 
 
-docker run --init --gpus=all  --rm -it --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -v /etc/X11/xorg.conf:/etc/X11/xorg.conf:ro -p $NO_VNC_PORT:40001 qeacourse/robodocker:selfcontained
+docker run --init --gpus=all  --rm --mount type=bind,source=/usr/local/MATLAB,target=/usr/local/MATLAB,readonly -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0:rw -v /etc/X11/xorg.conf:/etc/X11/xorg.conf:ro -p $NO_VNC_PORT:40001 qeacourse/robodocker:selfcontained
