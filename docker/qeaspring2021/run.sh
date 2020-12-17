ac525db6378ef8d5122a5a83dc8ebc39312f7fef
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-12-16 23:17:00 -0500

    added the new simulator

---
+++ docker/qeaspring2021/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+/opt/TurboVNC/bin/vncserver -SecurityTypes None
+DISPLAY_NUM=`/opt/TurboVNC/bin/vncserver -list | grep -m 1 "^:" | sed 's/^:\([0-9]*\).*/\1/'`
+export DISPLAY=:$DISPLAY_NUM
+xsetroot -solid grey
+/usr/bin/lxsession -s Lubuntu &
+
+let "VNC_PORT=5900 + $DISPLAY_NUM"
+
+# start Gazebo if needed
+if [ -z "$ROS_PKG" ]
+then
+    echo "not launching a world"
+else
+    # start MATLAB
+    matlab &
+    source ~/catkin_ws/devel/setup.bash
+    roslaunch $ROS_PKG $LAUNCH_FILE &
+fi
+
+# 3. start noVNC
+/noVNC-1.1.0/utils/launch.sh --vnc localhost:$VNC_PORT --listen 8081 --cert /root/self.pem
---
+++ docker/qeaspring2021/startup.m
@@ -0,0 +1,4 @@
+rosinit()
+addpath('/root/Sample_code');
+addpath('/root/matlab_boats');
+cd('/root/hosthome');
