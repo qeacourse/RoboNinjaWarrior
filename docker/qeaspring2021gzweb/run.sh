9362d42018b108c8a032bb76ac92c6400893d270
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-12-29 22:27:39 -0500

    add a version with gzweb

---
+++ docker/qeaspring2021gzweb/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+/opt/TurboVNC/bin/vncserver -SecurityTypes None
+DISPLAY_NUM=`/opt/TurboVNC/bin/vncserver -list | grep -m 1 "^:" | sed 's/^:\([0-9]*\).*/\1/'`
+export DISPLAY=:$DISPLAY_NUM
+xsetroot -solid grey
+/usr/bin/lxsession -s Lubuntu &
+
+let "VNC_PORT=5900 + $DISPLAY_NUM"
+
+# start Gazebo if needed
+if [ -z "$ROS_PKG" ]
+then
+    echo "not launching a world"
+else
+    # start MATLAB
+    cd /root
+    matlab &
+    source ~/catkin_ws/devel/setup.bash
+    (sleep 10; cd ~/gzweb && npm start) &
+    (sleep 5; roslaunch rosbridge_server rosbridge_websocket.launch) &
+    roslaunch $ROS_PKG $LAUNCH_FILE gui:=false &
+fi
+
+# 3. start noVNC
+/noVNC-1.1.0/utils/launch.sh --vnc localhost:$VNC_PORT --listen 8081 --cert /root/self.pem
