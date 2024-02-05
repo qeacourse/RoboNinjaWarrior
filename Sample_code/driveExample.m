803465b51b267fb833a80921cc14ecd7db650f17
paulruvolo <paullundyruvolo@gmail.com>
2024-02-05 17:07:33 -0500

    Add files via upload

---
+++ Sample_code/driveExample.m
@@ -0,0 +1,23 @@
+%define your parameters
+vl=0.1;
+vr=0.3;
+drivetime=7.7; %s
+
+%Connect to your Neato or the Simulator - choose one or the other
+%[sensors,vels]=neatoSim(); %uncomment for simulator
+[sensors,vels]=neato('192.168.16.106') %uncomment for physical neato
+fig = gcf;
+disp('press enter to continue');
+pause;
+
+tic %%start your timer in Matlab
+t=toc; %initiate t as the time since you started
+while t<drivetime
+    t=toc; %t update t
+    vels.lrWheelVelocitiesInMetersPerSecond=[vl,vr]; 
+    %pause(.01) %you can add a short delay to be safe/for communication lag in the physical neato. 
+end
+
+vels.lrWheelVelocitiesInMetersPerSecond=[0,0];
+pause(1);
+close(fig);
